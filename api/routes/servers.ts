a5579939fc5b86f6a1bc9d66022876980b78ef8d
yuto-moriizumi <moriizumi.yuto.18@shizuoka.ac.jp>
2021-03-11 19:12:41 +0900

    seleniumオプション情報をまとめた

--- api/routes/servers.ts
+++ api/routes/servers.ts
@@ -4,23 +4,33 @@ import { Options } from "selenium-webdriver/chrome";
 
 const router = express.Router();
 
+const SELENIUM_HOST = process.env.SELENIUM_HOST ?? "SELENIUM_HOST";
+const OPTIONS = [
+  "headless", //ヘッドレスの方が早い
+  "no-sandbox", //なんだっけ
+  "disable-dev-shm-usage", //docker使用時メモリ割り当てが少なくてもなんとかする
+  "user-agent=hogehoge", //UA確認で時間取られるのを防ぐ
+];
+
 // サーバリストを取得
 router.get("/", async (req, res) => {
+  console.log("buiiding...");
   const driver = await new Builder()
     .forBrowser("chrome")
-    .usingServer("http://selenium:4444/wd/hub")
-    .setChromeOptions(new Options().addArguments("--headless"))
+    .usingServer(SELENIUM_HOST)
+    .setChromeOptions(new Options().addArguments(...OPTIONS))
     .build();
+  console.log("buiid success");
   try {
     await driver.get(
       "https://battlelog.battlefield.com/bf4/ja/servers/pc/?filtered=1&expand=1&settings=&useLocation=1&useAdvanced=1&gameexpansions=-1&slots=1&slots=2&slots=4&q=&gameexpansions=-1&gameexpansions=-1&gameexpansions=-1&gameexpansions=-1&gameexpansions=-1&mapRotation=-1&modeRotation=-1&password=-1&regions=1&regions=32&osls=-1&vvsa=-1&vffi=-1&vaba=-1&vkca=-1&v3ca=-1&v3sp=-1&vmsp=-1&vrhe=-1&vhud=-1&vmin=-1&vnta=-1&vbdm-min=1&vbdm-max=300&vprt-min=1&vprt-max=300&vshe-min=1&vshe-max=300&vtkk-min=1&vtkk-max=99&vnit-min=30&vnit-max=86400&vtkc-min=1&vtkc-max=99&vvsd-min=0&vvsd-max=500&vgmc-min=0&vgmc-max=500"
     );
-    const el_results_container = await driver.findElement(By.id("results-container"));
-    const el_tbody = await driver.findElement(By.xpath("//tbody"));
+    // const el_results_container = await driver.findElement(By.id("results-container"));
+    // const el_tbody = await driver.findElement(By.xpath("//tbody"));
     const el_server_trs = await driver.findElements(By.xpath("//tr"));
     const tr_texts = (
       await Promise.all(
-        el_server_trs.map(async (el) => {
+        el_server_trs.map(async (el: any) => {
           const guid = await el.getAttribute("data-guid");
           if (!guid) return null;
           const texts = (await el.getText()).split("\n");
@@ -47,20 +57,14 @@ router.get("/", async (req, res) => {
 
 //サーバのチームとチケット数を返す
 router.get("/:guid", async (req, res) => {
+  console.log("buiiding...");
   const guid = req.params.guid;
   let driver: WebDriver | null = null;
   try {
     driver = await new Builder()
       .forBrowser("chrome")
-      .usingServer("http://selenium:4444")
-      .setChromeOptions(
-        new Options().addArguments(
-          "headless", //ヘッドレスの方が早い
-          "no-sandbox", //なんだっけ
-          "disable-dev-shm-usage", //docker使用時メモリ割り当てが少なくてもなんとかする
-          "user-agent=hogehoge" //UA確認で時間取られるのを防ぐ
-        )
-      )
+      .usingServer(SELENIUM_HOST)
+      .setChromeOptions(new Options().addArguments(...OPTIONS))
       .build();
     console.log("buiid success");
     await driver.get(`https://battlelog.battlefield.com/bf4/ja/servers/show/pc/${guid}`);
